f6a3d365928e540f8096cf90770d4bdf46504e19
Andrey Nemchaninov <tigranav@gmail.com>
2016-03-18 06:12:50 +0300

    главное приложение

---
+++ hello.py
@@ -0,0 +1,12 @@
+def app(environ, start_response):
+#	data = b"Hello, World!\n"
+#	for key in environ:
+#		data=data+"\n"+key
+#	data=data+"\n"+environ["QUERY_STRING"]
+	data = ''
+	q=environ["QUERY_STRING"]
+	for kv in q.split('&'):
+		data=data+kv+"\n"
+	start_response("200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data))) ])
+	return iter([data])
+
