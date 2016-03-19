ce70ea67fe4e63652a32e42e02490b2e2242f1a9
tigranav <tigranav@gmail.com>
2016-03-19 04:28:57 +0000

    проврен install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 ln -s /home/box/stepic-task2/etc/hello.py /etc/gunicorn.d/hello.py
-cp /home/box/stepic-task2/etc/ng.conf /etc/nginx/sites-available/ng.conf
-ln -s /etc/nginx/sites-available/ng.conf /etc/nginx/sites-enabled/ng.conf
+cp /home/box/stepic-task2/etc/default /etc/nginx/sites-available/default
+#ln -s /etc/nginx/sites-available/ng.conf /etc/nginx/sites-enabled/ng.conf
+/etc/init.s/nginx restart
 
-guinicorn -w 1 -b 0.0.0.0:8080 hello.py:app
+guinicorn -w 1 -b 0.0.0.0:8080 hello.py:app &
+
 wget -O 1.out http://localhost:8080/?a=b&b=c
+wget -O 2.out http://localhost/hello/?a=b&b=c
+
